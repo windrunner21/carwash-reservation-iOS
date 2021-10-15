ed3d1a35bf6e12e15df0f3af0d6897f803883242
Imran <haciyev008@gmail.com>
2021-10-16 02:14:13 +0400

    Initial Commit

---
+++ Washine/WashineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashineApp.swift
+//  Washine
+//
+//  Created by Imran on 16.10.21.
+//
+
+import SwiftUI
+
+@main
+struct WashineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
